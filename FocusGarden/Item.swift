8d455b7114cfda8929b8a75f9a756d3de4790501
Vedat Daglar <vedatdaglar@protonmail.com>
2026-03-15 00:05:21 +0300

    Initial Commit

---
+++ FocusGarden/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FocusGarden
+//
+//  Created by Vedat Dağlar on 15.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
